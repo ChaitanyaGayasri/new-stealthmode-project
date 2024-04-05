33e88213a62059445d6e8a9505a473080627b35f
ChaitanyaGayasri <chaitanyagayasri99@gmail.com>
2024-04-05 21:07:50 +0530

    Stealthmode

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import "./App.css"
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Dashboard from "./components/Dashboard"
+const App = () => {
+
+   return (
+   
+    <BrowserRouter>
+    <Routes>
+     <Route exact path="/" element={<Dashboard/>}/>
+     
+    </Routes>
+    </BrowserRouter>
+
+   )
+}
+
+export default App
